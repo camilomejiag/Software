95aea479a60e54c06f85cc81763c30090a482f7f
camilomejiag <camicasi@hotmail.com>
2017-05-30 14:46:18 -0800

    CRUD completo, faltan views de Pabl

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var Handlebars = require('handlebars');
 var router = express.Router();
 var db = require('monk')('localhost:27017/test');
 var userData  = db.get('userdata');
+var userRoutine = db.get('exercises');
+var globalId;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -21,7 +24,8 @@ router.post('/', function(req, res, next) {
       if (doc){
         var items = doc;
         res.render('index', {'items' : items});
-        console.log(items);
+        globalId = items._id;
+        console.log(globalId);
       }
 
     })
@@ -32,20 +36,50 @@ router.post('/', function(req, res, next) {
 //});
 
 router.post('/insert', function(req, res, next) {
+   //var pecho = req.body.pecho ? true : false;
   var item = {
     name: req.body.name,
     email: req.body.email,
     username: req.body.username,
-    password: req.body.password
+    password: req.body.password,
+    estado: 0,
+    injuries: {
+      pecho: req.body.pecho ? true : false,
+      espalda: req.body.espalda ? true : false,
+      bicep: req.body.bicep ? true : false,
+      tricep: req.body.tricep ? true : false,
+      hombro: req.body.hombro ? true : false,
+      pierna: req.body.pierna ? true : false,
+      abdomen: req.body.abdomen ? true : false
+
+    }
   };
-
   userData.insert(item);
-
   res.redirect('/');
 });
 
 router.get('/routines', function(req, res, next) {
   res.render('routines');
+});
+
+
+router.get('/profile', function(req, res, next) {
+  res.render('profile');
+});
+
+router.get('/get-data', function(req, res, next) {
+
+   var data = userData.find({},  function(err, doc){
+      if (doc){
+        var items = doc;
+        res.render('admin', {'items' : doc});
+      }
+
+    });
+  //var data = userData.find({});
+  //data.on('success', function(docs) {
+    //res.render('admin', {items: docs});
+  //});
 });
 
 router.post('/update', function(req, res, next) {
@@ -55,9 +89,10 @@ router.post('/update', function(req, res, next) {
     username: req.body.username,
     password: req.body.password
   };
-  var id = req.body.id;
+  //var id = req.body.id;
 
-  userData.update({"_id": db.id(id)}, item);
+  //userData.update({"username": username})
+  userData.update({"_id": db.id(globalId)}, item);
   res.render('index');
   //userData.updateById(id, item);
 });
@@ -70,7 +105,41 @@ router.post('/delete', function(req, res, next) {
   var id = req.body.username; 
    userData.remove({"username": id});
   //userData.removeById(id);
-  res.render('index');
+  res.render('admin');
 });
 
+//Handlebars Managements
+Handlebars.registerHelper('ifCond', function (v1, operator, v2, options) {
+
+    switch (operator) {
+        case '==':
+            return (v1 == v2) ? options.fn(this) : options.inverse(this);
+        case '===':
+            return (v1 === v2) ? options.fn(this) : options.inverse(this);
+        case '!=':
+            return (v1 != v2) ? options.fn(this) : options.inverse(this);
+        case '!==':
+            return (v1 !== v2) ? options.fn(this) : options.inverse(this);
+        case '<':
+            return (v1 < v2) ? options.fn(this) : options.inverse(this);
+        case '<=':
+            return (v1 <= v2) ? options.fn(this) : options.inverse(this);
+        case '>':
+            return (v1 > v2) ? options.fn(this) : options.inverse(this);
+        case '>=':
+            return (v1 >= v2) ? options.fn(this) : options.inverse(this);
+        case '&&':
+            return (v1 && v2) ? options.fn(this) : options.inverse(this);
+        case '||':
+            return (v1 || v2) ? options.fn(this) : options.inverse(this);
+        default:
+            return options.inverse(this);
+    }
+});
+
+
 module.exports = router;
+
+
+
+//Pecho, espalda, biceps, triceps, hombro, pierna, abdomen
