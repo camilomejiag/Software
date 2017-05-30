26cd04167a815a89f96320593c6720ace3216e95
camilomejiag <camicasi@hotmail.com>
2017-05-29 21:31:10 -0800

    CRUD (Read mas o menos)

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,7 @@
 var express = require('express');
-var passport = require('passport');
 var router = express.Router();
-var mongo = require('mongodb').MongoClient;
-var objectId = require('mongodb').ObjectID;
-var assert = require('assert');
-
-var url = 'mongodb://localhost:27017/test';
+var db = require('monk')('localhost:27017/test');
+var userData  = db.get('userdata');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -20,88 +16,61 @@ router.get('/registro', function(req, res, next) {
   res.render('registro');
 });
 
-router.get('/get-data', function(req, res, next) {
-  var resultArray = [];
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    var cursor = db.collection('userdata').find();
-    cursor.forEach(function(doc, err) {
-      assert.equal(null, err);
-      resultArray.push(doc);
-    }, function() {
-      db.close();
-      res.render('index', {items: resultArray});
-    });
-  });
-});
+router.post('/', function(req, res, next) {
+    var data = userData.findOne({username: req.body.username},  function(err, doc){
+      if (doc){
+        var items = doc;
+        res.render('index', {'items' : items});
+        console.log(items);
+      }
 
-/*router.post('/insert', function(req, res, next) {
+    })
+    //data.on('success', function(docs) {
+    //res.render('index', {items: data});
+    //console.log("error");
+  });
+//});
+
+router.post('/insert', function(req, res, next) {
   var item = {
     name: req.body.name,
     email: req.body.email,
     username: req.body.username,
     password: req.body.password
   };
-console.log(item);
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    db.collection('userdata').insertOne(item, function(err, result) {
-      assert.equal(null, err);
-      console.log('Item inserted');
-      db.close();
-    });
-  });
+
+  userData.insert(item);
 
   res.redirect('/');
-});*/
+});
 
-
-router.post('/insert', (req, res, next) => {
-    Account.register(new Account({ username : req.body.username }), req.body.password, (err, account) => {
-        if (err) {
-          return res.render('register', { error : err.message });
-        }
-
-        passport.authenticate('local')(req, res, () => {
-            req.session.save((err) => {
-                if (err) {
-                    return next(err);
-                }
-                res.redirect('/');
-            });
-        });
-    });
+router.get('/routines', function(req, res, next) {
+  res.render('routines');
 });
 
 router.post('/update', function(req, res, next) {
   var item = {
-    title: req.body.title,
-    content: req.body.content,
-    author: req.body.author
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
   };
   var id = req.body.id;
 
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    db.collection('user-data').updateOne({"_id": objectId(id)}, {$set: item}, function(err, result) {
-      assert.equal(null, err);
-      console.log('Item updated');
-      db.close();
-    });
-  });
+  userData.update({"_id": db.id(id)}, item);
+  res.render('index');
+  //userData.updateById(id, item);
+});
+
+router.get('/admin', function(req, res, next) {
+  res.render('admin');
 });
 
 router.post('/delete', function(req, res, next) {
-  var id = req.body.id;
-
-  mongo.connect(url, function(err, db) {
-    assert.equal(null, err);
-    db.collection('user-data').deleteOne({"_id": objectId(id)}, function(err, result) {
-      assert.equal(null, err);
-      console.log('Item deleted');
-      db.close();
-    });
-  });
+  var id = req.body.username; 
+   userData.remove({"username": id});
+  //userData.removeById(id);
+  res.render('index');
 });
 
 module.exports = router;
